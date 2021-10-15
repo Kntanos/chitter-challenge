c8152890670238d6bc54a4c80f01ad0cfe21e0d2
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-15 12:52:18 +0100

    Finalised setup with rspec-sinatra

---
+++ spec/feature/post_peep_spec.rb
@@ -0,0 +1,11 @@
+feature 'Post a peep' do
+  
+  scenario 'a user can add a peep' do
+    visit '/'
+    fill_in 'peep', with: 'Hello chitters'
+    click_button 'Chit your peep'
+
+    expect(current_path).to eq '/peep/index'
+    expecct(page).to have_content 'Hello chitters'
+  end
+end
