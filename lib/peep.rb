2338b9851fa220f285ae354926eaf9cd12d791c9
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-16 18:22:42 +0100

    Fixed peeps listed in reverse chronological order

--- lib/peep.rb
+++ lib/peep.rb
@@ -9,7 +9,7 @@ class Peep
       connection = PG.connect(dbname: 'chitter')
     end
 
-    result = connection.exec("SELECT * FROM peeps")
+    result = connection.exec("SELECT * FROM peeps ORDER BY created_at DESC")
     result.map { |peep| peep['content']}
   end
 
