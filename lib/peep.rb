18e6dea190455bbe488942e67006aa530148bff0
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-16 20:28:27 +0100

    Wrapped peeps, one view error to fix

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,6 +2,14 @@ require 'pg'
 
 class Peep
 
+  attr_reader :id, :content, :created_at
+
+  def initialize(id:, content:, created_at:)
+    @id = id
+    @content = content
+    @created_at = created_at
+  end
+
   def self.all
     if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
@@ -10,7 +18,9 @@ class Peep
     end
 
     result = connection.exec("SELECT * FROM peeps ORDER BY created_at DESC")
-    result.map { |peep| peep['content']}
+    result.map do |peep|
+      Peep.new(id: peep['id'], content: peep['content'], created_at: peep['created_at'])
+    end
   end
 
   def self.create(content:)
@@ -20,6 +30,7 @@ class Peep
       connection = PG.connect(dbname: 'chitter')
     end
 
-    connection.exec_params("INSERT INTO peeps (content) VALUES($1);", [content])
+    result = connection.exec_params("INSERT INTO peeps (content) VALUES($1) RETURNING id, content, created_at;", [content])
+    Peep.new(id: result[0]['id'], content: result[0]['content'], created_at: result[0]['created_at'])
   end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,7 +1,6 @@
 require 'peep'
 require 'pg'
 
-
 describe Peep do
 
   describe '.create' do
@@ -15,15 +14,26 @@ describe Peep do
   describe '.all' do
     it 'should list all peeps in reverse chronological order' do
       connection = PG.connect(dbname: 'chitter_test')
-      connection.exec("INSERT INTO peeps VALUES(1, 'I am happy', '2021-10-16 10:17:30');")
-      connection.exec("INSERT INTO peeps VALUES(2, 'I am ok', '2021-10-16 10:17:35');")
-      connection.exec("INSERT INTO peeps VALUES(3, 'I am fine', '2021-10-16 10:17:40');")
+      # connection.exec("INSERT INTO peeps VALUES(1, 'I am happy', '2021-10-16 10:17:30');")
+      # connection.exec("INSERT INTO peeps VALUES(2, 'I am ok', '2021-10-16 10:17:35');")
+      # connection.exec("INSERT INTO peeps VALUES(3, 'I am fine', '2021-10-16 10:17:40');")
+
+      Peep.create(content: 'I am happy')
+      Peep.create(content: 'I am ok')
+      peep = Peep.create(content: 'I am fine')
       
       peeps = Peep.all
 
-      expect(peeps.first).to eq("I am fine")
-      expect(peeps[1]).to eq("I am ok")
-      expect(peeps.last).to eq("I am happy") 
+      # expect(peeps.first).to eq("I am fine")
+      # expect(peeps[1]).to eq("I am ok")
+      # expect(peeps.last).to eq("I am happy") 
+
+      expect(peeps.length).to eq 3
+      expect(peeps.first).to be_a Peep
+      expect(peeps.first.id).to eq peep.id
+      expect(peeps.first.content).to eq 'I am fine'
+      expect(peeps.last.content).to eq 'I am happy'
+      expect(peeps.first.created_at).to eq peep.created_at
     end
   end
 end
