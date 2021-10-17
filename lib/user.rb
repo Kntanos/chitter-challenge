d0f5ea46dace63c66106b0aed0aadadc28fbadf8
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-17 16:49:31 +0100

    Implemented user sign up and tests pass

--- lib/user.rb
+++ lib/user.rb
@@ -13,9 +13,20 @@ attr_reader :id, :email, :password, :name, :username
   end
 
   def self.create(email:, password:, name:, username:)
-    connection = PG.connect(dbname: 'chitter_test')
-    result = connection.exec_params("INSERT INTO users (email, password, name, username) VALUES($1, $2, $3, $4) RETURNING id, email, password, name, username;", [email, password, name, username])
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
 
-    User.new(id: result[0]['id'], email: result[0]['email'], password: result[0]['password'], name: result[0]['name'], username: result[0]['username']) 
+    result = connection.exec_params("INSERT INTO users (email, password, name, username)
+    VALUES($1, $2, $3, $4)
+    RETURNING id, email, password, name, username;", [email, password, name, username])
+
+    User.new(id: result[0]['id'],
+      email: result[0]['email'],
+      password: result[0]['password'],
+      name: result[0]['name'],
+      username: result[0]['username']) 
   end
 end
